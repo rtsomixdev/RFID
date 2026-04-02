b37e089e4e45af7fba4e41148a73855ca40fdcfd
RTsoMix <rtsomixdev@hotmail.com>
2026-04-02 23:18:53 +0700

    THESIS RFID

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
     allowedHosts: true, 
     proxy: {
       '/api': {
-        // 👇 แก้ตรงนี้: เปลี่ยนจาก http://linen_api:8080 เป็น localhost
-        // ⚠️ สำคัญ: เลข 5134 ต้องตรงกับที่ขึ้นใน Terminal ตอนคุณรัน 'dotnet watch run'
+        //แก้ตรงนี้: เปลี่ยนจาก http://linen_api:8080 เป็น localhost
+        //สำคัญ: เลข 5134 ต้องตรงกับที่ขึ้นใน Terminal ตอนคุณรัน 'dotnet watch run'
         // (ถ้ามันขึ้น port อื่น เช่น 5000, 5200 ให้แก้เลขตรงนี้ตามครับ)
         target: 'http://localhost:5134', 
         changeOrigin: true,
